6e0f982ee84b9a8884d37c3e2ed19f7811fa6fcd
PamelaMoreno <parker.moreno15@gmail.com>
2022-04-20 14:13:36 -0500

    Actualizacion 2:08 p.m

--- converter.php
+++ converter.php
@@ -6,19 +6,29 @@ $opciones = $_POST["opciones"];
 
 switch ($opciones) {
     case "var1":
-	    $resultado = $unidad*0.03381;
-	    echo $resultado . " onzas fluidas";
+	    $resultado = $unidad/29.574;
+	    echo number_format($resultado, 2, '.', '.') . " onzas fluidas";
 	    break;
-    //case "var2":
-     //   echo "i es igual a 1";
-    //case "var3":
-     //   echo "i es igual a 2";
-     //case "var4":
-      //  echo "i es igual a 2";
-     //case "var5":
-      //  echo "i es igual a 2";
-     //case "var6":
-      //  echo "i es igual a 2";
+    case "var2":
+	    $resultado = $unidad*1.094;
+	    echo number_format($resultado, 2, '.', '.') . " yardas";
+	    break;
+    case "var3":
+	    $resultado = $unidad/454;
+	    echo number_format($resultado, 2, '.', '.') . " libras";
+	    break;
+    case "var4":
+	    $resultado = ($unidad*9/5)+32;
+	    echo number_format($resultado, 2, '.', '.') . " fahrenheit";
+	    break;
+    case "var5":
+	    $resultado = $unidad/1.609;
+	    echo number_format($resultado, 2, '.', '.') . " millas";
+	    break;
+    case "var6":
+	    $resultado = $unidad/26.1405;
+	    echo number_format($resultado, 2, '.', '.')  . " libras esterlinas";
+	    break;
 }
 ?>
 
